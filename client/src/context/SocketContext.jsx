0d9595bb943870a8675931873e0bf2dc1015103f
Zohaib Akhtar <zohaibakhtar336@gmail.com>
2024-03-31 12:51:51 +0530

    socket url updated0

--- client/src/context/SocketContext.jsx
+++ client/src/context/SocketContext.jsx
@@ -15,7 +15,7 @@ export const SocketContextProvider = ({ children }) => {
 
 	useEffect(() => {
 		if (authUser) {
-			const socket = io("https://fullstack-chatapp-project.onrender.com/", {
+			const socket = io("https://fullstack-chatapp-project.onrender.com", {
 				query: {
 					userId: authUser._id,
 				},
